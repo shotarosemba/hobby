9cc8351032e4f40be07232a91cb58a43fb473661
shotarosemba <shotaro.semba@gmail.com>
2023-06-13 15:37:03 +0900

    update package.json

--- database/migrations/2023_05_18_225732_create_works_table.php
+++ database/migrations/2023_05_18_225732_create_works_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('works_', function (Blueprint $table) {
+        Schema::create('works', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->string('name',100);
